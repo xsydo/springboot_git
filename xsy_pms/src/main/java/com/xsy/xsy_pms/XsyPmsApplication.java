0ca9a6e0cb32d046ad66fa510e95fbbc5ac2c0ee
xsy <1903414588@qq.com>
2020-07-09 20:45:16 +0800

    project_test

--- xsy_pms/src/main/java/com/xsy/xsy_pms/XsyPmsApplication.java
+++ xsy_pms/src/main/java/com/xsy/xsy_pms/XsyPmsApplication.java
@@ -1,9 +1,13 @@
 package com.xsy.xsy_pms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.xsy.xsy_pms.dao"}) //扫描DAO
+@ServletComponentScan("com.xsy.hr.filters")
 public class XsyPmsApplication {
 
     public static void main(String[] args) {
