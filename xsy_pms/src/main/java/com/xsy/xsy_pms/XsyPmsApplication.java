6a96eb98227a4e5ae992cdc2cd24d3460c7ce2d6
xsy <1903414588@qq.com>
2020-07-09 16:38:16 +0800

    project_test

---
+++ xsy_pms/src/main/java/com/xsy/xsy_pms/XsyPmsApplication.java
@@ -0,0 +1,13 @@
+package com.xsy.xsy_pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsyPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsyPmsApplication.class, args);
+    }
+
+}
---
+++ xsy_pms/src/test/java/com/xsy/xsy_pms/XsyPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsy.xsy_pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsyPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
